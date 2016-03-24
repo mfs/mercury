6aaa6e187c466a36dfa7cfbc3e21c77158f73153
Mike Sampson <mike@sambodata.com>
2016-03-24 22:07:02 +1100

    Draw a rectangle.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,14 @@ fn main() {
     let window: PistonWindow = WindowSettings::new("mercury", [600, 600])
         .exit_on_esc(true).build().unwrap();
 
-    for _e in window {
+    for e in window {
+        e.draw_2d(|c, g| {
+            clear([0.0, 0.0, 0.0, 1.0], g);
+            let center = c.transform.trans(300.0, 300.0);
+            let square = rectangle::square(0.0, 0.0, 100.0);
+            let gray = [0.7, 0.7, 0.7, 1.0];
+            rectangle(gray, square, center.trans(-50.0, -50.0), g);
+        });
 
     }
 }
