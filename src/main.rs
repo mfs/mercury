73622633066c9e7bde9c7b253caa1af552dd9c52
Mike Sampson <mike@sambodata.com>
2016-03-25 21:12:18 +1100

    Add keyboard movement.

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,33 @@ use piston_window::*;
 
 struct Game {
     rotation: f64,
+    x: f64, y: f64,
+    up_d: bool, down_d: bool, left_d: bool, right_d: bool,
 }
 
 impl Game {
     fn new() -> Game {
-        Game { rotation: 0.0 }
+        Game {
+            rotation: 0.0, x: 0.0, y: 0.0,
+            up_d: false, down_d: false, left_d: false, right_d: false
+        }
     }
 
     fn on_update(&mut self, update: UpdateArgs) {
         self.rotation += 0.7 * update.dt;
+
+        if self.up_d {
+           self.y += (-50.0) * update.dt;
+        }
+        if self.down_d {
+            self.y += (50.0) * update.dt;
+        }
+        if self.left_d {
+            self.x += (-50.0) * update.dt;
+        }
+        if self.right_d {
+            self.x += (50.0) * update.dt;
+        }
     }
 
     fn on_draw(&mut self, render: RenderArgs, e: PistonWindow) {
@@ -21,9 +39,49 @@ impl Game {
             let center = c.transform.trans((render.width / 2) as f64, (render.height / 2) as f64);
             let square = rectangle::square(0.0, 0.0, 100.0);
             let gray = [0.7, 0.7, 0.7, 1.0];
-            rectangle(gray, square, center.rot_rad(self.rotation).trans(-50.0, -50.0), g);
+            rectangle(gray, square, center.trans(self.x, self.y).rot_rad(self.rotation).trans(-50.0, -50.0), g);
         });
     }
+
+    fn on_input(&mut self, input: Input) {
+   	    match input {
+			Input::Press(but) => {
+				match but {
+					Button::Keyboard(Key::Up) => {
+					    self.up_d = true;
+					}
+					Button::Keyboard(Key::Down) => {
+					    self.down_d = true;
+					}
+					Button::Keyboard(Key::Left) => {
+					    self.left_d = true;
+					}
+					Button::Keyboard(Key::Right) => {
+					    self.right_d = true;
+					}
+					_ => {}
+				}
+			}
+			Input::Release(but) => {
+				match but {
+					Button::Keyboard(Key::Up) => {
+					    self.up_d = false;
+					}
+					Button::Keyboard(Key::Down) => {
+					    self.down_d = false;
+					}
+					Button::Keyboard(Key::Left) => {
+					    self.left_d = false;
+					}
+					Button::Keyboard(Key::Right) => {
+					    self.right_d = false;
+					}
+					_ => {}
+				}
+			}
+			_ => {}
+		}
+	}
 }
 
 fn main() {
@@ -37,6 +95,7 @@ fn main() {
         match e.event {
             Some(Event::Update(update)) => game.on_update(update),
             Some(Event::Render(render)) => game.on_draw(render, e),
+            Some(Event::Input(input))   => game.on_input(input),
             _ => {}
         }
     }
